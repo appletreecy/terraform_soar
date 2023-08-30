eea9a8ea73917369e583c2583433a6bb109585a8
Steven Cao <appletreecy@gmail.com>
2023-08-30 15:21:39 +1000

    2023-0830

--- main.tf
+++ main.tf
@@ -47,8 +47,25 @@ resource "aws_subnet" "subnet-1" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "prod-subnet"
+    Name = "prod-subnet-1"
   }
+}
+
+# Create another subnet
+resource "aws_subnet" "subnet-2" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "prod-subnet-2"
+  }
+}
+
+# Create the subnet Group
+resource "aws_db_subnet_group" "db_subnet_group_1" {
+  name       = "db-subnet-group-1"
+  subnet_ids = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
 }
 
 # 5. Associate subnet with Route Table
@@ -56,6 +73,13 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet-1.id
   route_table_id = aws_route_table.prod-route-table.id
 }
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.subnet-2.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+
 # 6. Create Security Group to allow port 22,80,443
 resource "aws_security_group" "allow_web" {
   name        = "allow_web_traffic"
@@ -84,6 +108,22 @@ resource "aws_security_group" "allow_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Postgres-port"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "all-port"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -104,10 +144,15 @@ resource "aws_network_interface" "web-server-nic" {
   security_groups = [aws_security_group.allow_web.id]
 
 }
-# 8. Assign an elastic IP to the network interface created in step 7
 
+# 7.5 Sleep for 10 seconds
+# resource "time_sleep" "wait_10_seconds" {
+#   create_duration = "10s"
+# }
+
+# 8. Assign an elastic IP to the network interface created in step 7
 resource "aws_eip" "one" {
-  vpc                       = true
+  domain                    = "vpc"
   network_interface         = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
   depends_on                = [aws_internet_gateway.gw]
@@ -117,6 +162,7 @@ output "server_public_ip" {
   value = aws_eip.one.public_ip
 }
 
+
 # 9. Create Ubuntu server and install/enable apache2
 
 resource "aws_instance" "web-server-instance" {
@@ -124,18 +170,44 @@ resource "aws_instance" "web-server-instance" {
   instance_type     = "m5.xlarge"
   availability_zone = "us-east-1a"
   key_name          = "2023-0804"
+  depends_on        = [aws_eip.one]
 
   network_interface {
     device_index         = 0
     network_interface_id = aws_network_interface.web-server-nic.id
   }
 
+  provisioner "file" {
+    source      = "2023-0804.pem"
+    destination = "/home/phantom/.ssh/2023-0804.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/phantom/.ssh/2023-0804.pem",
+      "sudo yum -y install epel-release",
+      "sudo yum -y install ansible",
+    ]
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+
+  }
+
   tags = {
     Name = "soar-server"
   }
 }
-
-
 
 output "server_private_ip" {
   value = aws_instance.web-server-instance.private_ip
@@ -144,4 +216,445 @@ output "server_private_ip" {
 
 output "server_id" {
   value = aws_instance.web-server-instance.id
+}
+
+# Setup the second SOAR server
+# Create a network interface with an ip in the subnet 
+
+resource "aws_network_interface" "soar-server-nic-1" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.51"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+# Assign an elastic IP to the network interface
+resource "aws_eip" "one-1" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.soar-server-nic-1.id
+  associate_with_private_ip = "10.0.1.51"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip-1" {
+  value = aws_eip.one-1.public_ip
+}
+
+# Create SOAR server-1
+
+resource "aws_instance" "soar-server-instance-1" {
+  ami               = "ami-0b18e0c38e02712f1"
+  instance_type     = "m5.xlarge"
+  availability_zone = "us-east-1a"
+  key_name          = "2023-0804"
+  depends_on        = [aws_eip.one-1]
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.soar-server-nic-1.id
+  }
+
+  provisioner "file" {
+    source      = "2023-0804.pem"
+    destination = "/home/phantom/.ssh/2023-0804.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/phantom/.ssh/2023-0804.pem"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+  }
+
+  tags = {
+    Name = "soar-server-1"
+  }
+
+}
+
+output "server_private_ip-1" {
+  value = aws_instance.soar-server-instance-1.private_ip
+
+}
+
+output "server_id-1" {
+  value = aws_instance.soar-server-instance-1.id
+}
+
+# Setup the third SOAR server
+# Create a network interface with an ip in the subnet 
+
+resource "aws_network_interface" "soar-server-nic-2" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.52"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+# Assign an elastic IP to the network interface
+resource "aws_eip" "one-2" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.soar-server-nic-2.id
+  associate_with_private_ip = "10.0.1.52"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip-2" {
+  value = aws_eip.one-2.public_ip
+}
+
+# Create SOAR server-2
+
+resource "aws_instance" "soar-server-instance-2" {
+  ami               = "ami-0b18e0c38e02712f1"
+  instance_type     = "m5.xlarge"
+  availability_zone = "us-east-1a"
+  key_name          = "2023-0804"
+  depends_on        = [aws_eip.one-2]
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.soar-server-nic-2.id
+  }
+
+  provisioner "file" {
+    source      = "2023-0804.pem"
+    destination = "/home/phantom/.ssh/2023-0804.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/phantom/.ssh/2023-0804.pem"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+
+  }
+
+  tags = {
+    Name = "soar-server-2"
+  }
+}
+
+# Setup the forth SOAR server
+# Create a network interface with an ip in the subnet 
+
+resource "aws_network_interface" "soar-server-nic-3" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.53"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+# Assign an elastic IP to the network interface
+resource "aws_eip" "one-3" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.soar-server-nic-3.id
+  associate_with_private_ip = "10.0.1.53"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip-3" {
+  value = aws_eip.one-3.public_ip
+}
+
+# Create SOAR server-3
+
+resource "aws_instance" "soar-server-instance-3" {
+  ami               = "ami-0b18e0c38e02712f1"
+  instance_type     = "m5.xlarge"
+  availability_zone = "us-east-1a"
+  key_name          = "2023-0804"
+  depends_on        = [aws_eip.one-3]
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.soar-server-nic-3.id
+  }
+
+  provisioner "file" {
+    source      = "2023-0804.pem"
+    destination = "/home/phantom/.ssh/2023-0804.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/phantom/.ssh/2023-0804.pem"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+
+  }
+
+  tags = {
+    Name = "soar-server-3"
+  }
+}
+
+output "server_private_ip-3" {
+  value = aws_instance.soar-server-instance-3.private_ip
+
+}
+
+output "server_id-3" {
+  value = aws_instance.soar-server-instance-3.id
+}
+
+# Setup the fifth SOAR server
+# Create a network interface with an ip in the subnet 
+
+resource "aws_network_interface" "soar-server-nic-4" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.54"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+# Assign an elastic IP to the network interface
+resource "aws_eip" "one-4" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.soar-server-nic-4.id
+  associate_with_private_ip = "10.0.1.54"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip-4" {
+  value = aws_eip.one-4.public_ip
+}
+
+# Create SOAR server-4
+
+resource "aws_instance" "soar-server-instance-4" {
+  ami               = "ami-0b18e0c38e02712f1"
+  instance_type     = "m5.xlarge"
+  availability_zone = "us-east-1a"
+  key_name          = "2023-0804"
+  depends_on        = [aws_eip.one-4]
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.soar-server-nic-4.id
+  }
+
+  provisioner "file" {
+    source      = "2023-0804.pem"
+    destination = "/home/phantom/.ssh/2023-0804.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/phantom/.ssh/2023-0804.pem"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "phantom"
+      private_key = file("2023-0804.pem")
+      host        = self.public_ip
+    }
+
+  }
+
+  tags = {
+    Name = "soar-server-4"
+  }
+}
+
+output "server_private_ip-4" {
+  value = aws_instance.soar-server-instance-4.private_ip
+
+}
+
+output "server_id-4" {
+  value = aws_instance.soar-server-instance-4.id
+}
+
+output "server_private_ip-2" {
+  value = aws_instance.soar-server-instance-2.private_ip
+
+}
+
+output "server_id-2" {
+  value = aws_instance.soar-server-instance-2.id
+}
+
+# Create a new Application load balancer 
+resource "aws_lb" "soar-lb" {
+  name               = "test-lb-soar"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_web.id]
+  subnets            = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
+
+  enable_deletion_protection = false
+
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+#Configure the target group
+resource "aws_lb_target_group" "https_service" {
+  name     = "target-group-for-https-service"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = aws_vpc.prod-vpc.id
+
+
+
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 604800 # Session duration in seconds
+  }
+
+  health_check {
+    path     = "/check" # Replace with the health check path for your application
+    port     = 443      # Port to perform health checks on
+    protocol = "HTTPS"
+  }
+}
+# attach the EC2 instance to the target group
+
+resource "aws_lb_target_group_attachment" "attchment-1" {
+  target_group_arn = aws_lb_target_group.https_service.arn
+  target_id        = aws_instance.soar-server-instance-1.id
+  port             = 443
+}
+
+resource "aws_lb_target_group_attachment" "attchment-2" {
+  target_group_arn = aws_lb_target_group.https_service.arn
+  target_id        = aws_instance.soar-server-instance-2.id
+  port             = 443
+}
+
+resource "aws_lb_target_group_attachment" "attchment-3" {
+  target_group_arn = aws_lb_target_group.https_service.arn
+  target_id        = aws_instance.soar-server-instance-3.id
+  port             = 443
+}
+
+#configure lb listener
+resource "aws_lb_listener" "front_end_listener" {
+  load_balancer_arn = aws_lb.soar-lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:768298382555:certificate/cee27c32-022a-4c16-9715-92baab8bbf8a"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.https_service.arn
+
+  }
+}
+
+# add one listener rule to the elb
+resource "aws_lb_listener_rule" "static-path" {
+  listener_arn = aws_lb_listener.front_end_listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.https_service.arn
+
+  }
+  condition {
+    path_pattern {
+      values = ["/websocket"]
+    }
+  }
+}
+
+#create the EFS File System
+resource "aws_efs_file_system" "soar-cluster-efs" {
+  creation_token = "soar-efs"
+
+  tags = {
+    Name = "soar-efs-name"
+  }
+}
+
+#create the efs mount target
+resource "aws_efs_mount_target" "alpha" {
+  file_system_id  = aws_efs_file_system.soar-cluster-efs.id
+  subnet_id       = aws_subnet.subnet-1.id
+  security_groups = [aws_security_group.allow_web.id]
+  ip_address      = "10.0.1.98"
+}
+
+#create the efs access point
+resource "aws_efs_access_point" "soar-efs-access-point" {
+  file_system_id = aws_efs_file_system.soar-cluster-efs.id
+
+}
+
+
+#create the RDS
+resource "aws_db_instance" "default" {
+  allocated_storage     = 500
+  max_allocated_storage = 1000
+  db_name               = "phantom"
+  engine                = "postgres"
+  engine_version        = "11"
+  instance_class        = "db.t2.large"
+  username              = "postgres"
+  password              = "splunk3du"
+  parameter_group_name  = "default.postgres11"
+  skip_final_snapshot   = true
+  multi_az              = false
+  publicly_accessible   = false
+
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group_1.name
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
+
+}
+
+output "rds_db_address" {
+  value = aws_db_instance.default.address
 }
