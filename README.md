d973d3c4ac88c2bb78cef47a4e37817854dde6c7
Steven Cao <appletreecy@gmail.com>
2023-09-05 10:59:34 +1000

    20230905

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # terraform_soar
-terraform_soar repository 
+
+terraform_soar repository
+
+1. Export the following two environment variable
+   access_key
+   secret_key
+
+2. clone the git hub repository
+
+3. Run Terraform apply
+   ./terraform apply
--- scao_terraform_apply.sh
+++ scao_terraform_apply.sh
@@ -7,5 +7,7 @@ terraform apply -auto-approve
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
+execution_time_mins = $((end_time - start_time)/60)
 
 echo "Terraform script execution time: ${execution_time} seconds"
+echo "Terraform script execution time: ${execution_time_mins} mins"
